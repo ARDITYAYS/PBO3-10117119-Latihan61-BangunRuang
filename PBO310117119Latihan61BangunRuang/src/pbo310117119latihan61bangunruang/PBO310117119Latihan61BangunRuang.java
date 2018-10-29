2f0d3fee28adcad35cb955a63a8ad92d0e23027d
Arditya Yogaputra S <43446706+ARDITYAYS@users.noreply.github.com>
2018-10-30 00:39:00 +0700

    Add files via upload

---
+++ PBO310117119Latihan61BangunRuang/src/pbo310117119latihan61bangunruang/PBO310117119Latihan61BangunRuang.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan61bangunruang;
+
+/**
+ *
+ * @author Yogaputra
+ * Nama                 : Arditya Yogaputra S
+ * Kelas                : PBO3
+ * NIM                  : 10117119
+ * Deskripsi Program    : Program ini  menampilkan bangun ruang
+ */
+public class PBO310117119Latihan61BangunRuang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bola b = new Bola();
+        Tabung t = new Tabung();
+        Kerucut k = new Kerucut();
+
+        b.setR(7);
+        System.out.println("Volume Bola : " + Math.ceil(b.hitungVolume()));
+
+        t.setH(21);
+        t.setR(10);
+        System.out.println("Volume Tabung : " + Math.ceil(t.hitungVolume()));
+
+        k.setR(14);
+        k.setH(9);
+        System.out.println("Volume Kerucut : " + Math.ceil(k.hitungVolume()));
+    }
+
+}
